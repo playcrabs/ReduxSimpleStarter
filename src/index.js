7f40d4e9fd1b1b6edad20d3edef329a36570d352
playcrabs <yongsin1ok@naver.com>
2020-09-07 20:29:17 +0900

    200907

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
 import App from './components/app';
 import reducers from './reducers';
