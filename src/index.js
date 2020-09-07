57dee107424d49d99a6b6692b82b4f93c6022c9c
playcrabs <yongsin1ok@naver.com>
2020-09-07 20:36:01 +0900

    0907

--- src/index.js
+++ src/index.js
@@ -1,13 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+//리액트 모듈 불러오기
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import YTSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoList from "./components/video_list";
+const API_KEY = "AIzaSyBlKirhuD2GibXHPtBdNXinET_E2t1ykTk";
 
-import App from './components/app';
-import reducers from './reducers';
+class App extends Component {
+  constructor(props) {
+    super(props);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+    this.state = { videos: [] };
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+    YTSearch({ key: API_KEY, term: "K/DA" }, (videos) => {
+      this.setState({ videos });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector(".container"));
